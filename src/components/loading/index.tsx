8dc4468d7031ff31f43d19feea741511b94e488b
Elias Forte <eliasforte.dev@gmail.com>
2022-08-30 17:49:38 -0300

    feat(component): created `Loading`

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Center, Spinner } from '@chakra-ui/react';
+
+export const Loading: React.FC = () => (
+  <Center height="100vh">
+    Carregando <Spinner size="lg" />
+  </Center>
+);
