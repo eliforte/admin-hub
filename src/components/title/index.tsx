6ce6d1ee929c96b46ce60190d1a6f93411ae5c27
Elias Forte <eliasforte.dev@gmail.com>
2022-08-30 17:50:32 -0300

    feat(component): created `Title`

---
+++ src/components/title/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box, Heading, Button } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+
+export const Title: React.FC = () => {
+  const Navigate = useNavigate();
+
+  return (
+    <Box
+      m="10px"
+    >
+      <Button
+        bg="transparent"
+        _hover={{ bg: 'transparent' }}
+        onClick={() => Navigate('/home/servicos')}
+      >
+        <Heading color="white">Admin Hub</Heading>
+      </Button>
+    </Box>
+  );
+};
