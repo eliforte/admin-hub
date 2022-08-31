65bfc3962ef4967f5a48021ef266319f8418b4c3
Elias Forte <eliasforte.dev@gmail.com>
2022-08-31 02:24:28 -0300

    feat(component): validation forms and handleSubmit

--- src/components/newService/index.tsx
+++ src/components/newService/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { AxiosError } from 'axios';
 import {
   Input,
-  // Text,
-  // Spinner,
+  Text,
+  Spinner,
   Select,
-  // useToast,
+  useToast,
+  Button,
   FormLabel,
   Tag,
   InputGroup,
@@ -16,41 +18,76 @@ import {
   Box,
   AccordionPanel,
 } from '@chakra-ui/react';
-// import { Add } from '@mui/icons-material/';
-// import api from '../../services/api';
+import { Add } from '@mui/icons-material/';
+import api from '../../services/api';
 
 interface IService {
   type: string;
   pacient_fullname: string;
   plan: string;
-  payment_method: string;
-  form_of_payment: string;
   quantity_installments: number;
   total: number;
   quantity_installments_paid: number;
   payment_day: number | null;
   last_payment: Date | null;
   next_payment: Date | null;
-  installment_value: number;
 }
 
 export const NewService: React.FC = () => {
-  // const [loading, setLoading] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const [service, setService] = React.useState<IService>({
     type: '',
     pacient_fullname: '',
     plan: '',
-    payment_method: 'Cartão de crédito',
-    form_of_payment: 'À vista',
     quantity_installments: 2,
     total: 0,
     quantity_installments_paid: 0,
-    payment_day: null,
+    payment_day: 5,
     last_payment: null,
     next_payment: null,
-    installment_value: 0,
   });
-  // const toast = useToast();
+  const [paymentMethod, setPaymentMethod] = React.useState('Cartão de crédito');
+  const [formOfPayment, setFormOfPayment] = React.useState('À vista');
+  const [installmentValue, setInstallmentValue] = React.useState(0);
+  const [totalForm, setTotalForm] = React.useState(0);
+  const toast = useToast();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const token = JSON.parse(String(sessionStorage.getItem('user')));
+      await api.post('/voucher', {
+        ...service,
+        payment_method: paymentMethod,
+        form_of_payment: formOfPayment,
+        installment_value: installmentValue,
+      }, { headers: { Authorization: `Bearer ${token}` } });
+      setLoading(false);
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        setLoading(false);
+        return toast({
+          position: 'top',
+          title: 'Ops!',
+          description: err.response?.data?.message,
+          status: 'warning',
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+      setLoading(false);
+      toast({
+        position: 'top',
+        title: 'Ops!',
+        description: 'Algo deu errado, tente novamente.',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+      console.log(err);
+    }
+  };
 
   const paymentMethods: { [key: string]: string; }[] = [
     { value: 'Cartão de crédito', label: 'Cartão de crédito' },
@@ -58,56 +95,85 @@ export const NewService: React.FC = () => {
     { value: 'Em dinheiro', label: 'Em dinheiro' },
   ];
 
-  const formOfPayment: { [key: string]: string; }[] = [
+  const formOfPaymentOptions: { [key: string]: string; }[] = [
     { value: 'À vista', label: 'À vista' },
     { value: 'Parcelamento', label: 'Parcelamento' },
   ];
+
+  const paymentDays = [5, 8, 15, 18];
 
   const quantityInstallments = (): number[] => {
     const newQuantity = Array.from({ length: 12 }, (_, index) => index + 1);
     return newQuantity;
   };
 
-  const paymentDays = [5, 8, 15, 18];
-
   const calculateInstallmentValue = () => {
-    const fees = service.total * 0.03 * service.quantity_installments;
-    const result = Number(((fees + service.total) / service.quantity_installments).toFixed(2));
-    setService({ ...service, installment_value: result });
+    const fees = totalForm * 0.03 * service.quantity_installments;
+    const result = Number(((fees + totalForm) / service.quantity_installments).toFixed(2));
+    setInstallmentValue(result);
     return result;
   };
 
+  const calculateTotal = () => {
+    if (
+      paymentMethod === 'Cartão de crédito'
+      && formOfPayment === 'Parcelamento'
+      && totalForm > 0
+    ) {
+      const priceWithFees = service.quantity_installments * installmentValue;
+      setService({ ...service, total: priceWithFees });
+    } else {
+      setService({ ...service, total: totalForm });
+    }
+  };
+
   React.useEffect(() => {
-    if (service.quantity_installments > 1) calculateInstallmentValue();
-    if (service.form_of_payment === 'À vista') setService({ ...service, quantity_installments: 0 });
-  }, [service.quantity_installments, service.form_of_payment]);
+    totalForm > 0 && calculateTotal();
+    formOfPayment === 'Parcelamento' && calculateInstallmentValue();
+    if (paymentMethod !== 'Cartão de crédito') {
+      setInstallmentValue(totalForm);
+      setFormOfPayment('À vista');
+      setService({ ...service, quantity_installments: 2, total: totalForm });
+    }
+  }, [
+    service.quantity_installments,
+    paymentMethod,
+    formOfPayment,
+    installmentValue,
+    totalForm,
+  ]);
 
   return (
     <Accordion
       allowToggle
       backgroundColor="whitesmoke"
       borderRadius={5}
+      alignItems="center"
       mr={5}
     >
       <AccordionItem borderRadius={5}>
         <h2>
           <AccordionButton>
+            <Add />
             <Box flex="1" textAlign="left">
               Registrar novo serviço
             </Box>
           </AccordionButton>
         </h2>
-        <AccordionPanel borderRadius={5}>
-          <form>
+        <AccordionPanel width="100%" borderRadius={5}>
+          <form onSubmit={(e) => handleSubmit(e)}>
             <Grid
-              templateColumns={['1fr', '1fr 1fr', '1fr 1fr 1fr']}
-              gap={7}
-              w={['90%', '90%', '70%', '90%']}
+              templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(4, 1fr)']}
+              gap={4}
+              w="100%"
+              alignItems="center"
+              justifyItems="start"
             >
-              <GridItem>
-                <InputGroup flexDirection="column">
-                  <FormLabel>Nome do paciente (completo)</FormLabel>
+              <GridItem width="100%">
+                <InputGroup flexDirection="column" justifyContent="space-between">
+                  <FormLabel>Nome do paciente</FormLabel>
                   <Input
+                    isRequired
                     bg="gray.100"
                     maxWidth={370}
                     placeholder="Ex: Miguel Fernandez Viera"
@@ -120,6 +186,7 @@ export const NewService: React.FC = () => {
                 <InputGroup flexDirection="column">
                   <FormLabel>Tipo do Atendimento</FormLabel>
                   <Input
+                    isRequired
                     bg="gray.100"
                     placeholder="Ex: Extração de siso"
                     type="text"
@@ -131,10 +198,11 @@ export const NewService: React.FC = () => {
                 <InputGroup flexDirection="column">
                   <FormLabel>Valor</FormLabel>
                   <Input
+                    isRequired
                     bg="gray.100"
                     placeholder="Ex: 199.50"
                     type="number"
-                    onChange={(e) => setService({ ...service, total: Number(e.target.value) })}
+                    onChange={(e) => setTotalForm(Number(e.target.value))}
                   />
                 </InputGroup>
               </GridItem>
@@ -142,6 +210,7 @@ export const NewService: React.FC = () => {
                 <InputGroup flexDirection="column">
                   <FormLabel>Plano odontológico</FormLabel>
                   <Input
+                    isRequired
                     bg="gray.100"
                     placeholder="Ex: Odonto Care Plus"
                     type="text"
@@ -153,7 +222,8 @@ export const NewService: React.FC = () => {
                 <InputGroup flexDirection="column">
                   <FormLabel>Método de pagamento</FormLabel>
                   <Select
-                    onChange={(e) => setService({ ...service, payment_method: e.target.value })}
+                    isRequired
+                    onChange={(e) => setPaymentMethod(e.target.value)}
                     defaultValue={paymentMethods[0].value}
                   >
                     {
@@ -170,16 +240,17 @@ export const NewService: React.FC = () => {
                 </InputGroup>
               </GridItem>
               <GridItem
-                display={service.payment_method === 'Cartão de crédito' ? 'block' : 'none'}
+                display={paymentMethod === 'Cartão de crédito' ? 'block' : 'none'}
               >
                 <InputGroup flexDirection="column">
                   <FormLabel>Forma de pagamento</FormLabel>
                   <Select
-                    onChange={(e) => setService({ ...service, form_of_payment: e.target.value })}
-                    defaultValue={formOfPayment[0].value}
+                    isRequired
+                    onChange={(e) => setFormOfPayment(e.target.value)}
+                    value={formOfPayment}
                   >
                     {
-                      formOfPayment.map((formPayment, index) => (
+                      formOfPaymentOptions.map((formPayment, index) => (
                         <option
                           key={index.toString() + 1}
                           value={formPayment.value}
@@ -192,11 +263,17 @@ export const NewService: React.FC = () => {
                 </InputGroup>
               </GridItem>
               <GridItem
-                display={service.form_of_payment === 'Parcelamento' ? 'block' : 'none'}
+                display={
+                  formOfPayment === 'Parcelamento'
+                  && paymentMethod === 'Cartão de crédito'
+                    ? 'block'
+                    : 'none'
+                }
               >
-                <InputGroup>
-                  <FormLabel>Quantidade de parcelas</FormLabel>
+                <InputGroup flexDirection="column">
+                  <FormLabel>Qnt. de parcelas</FormLabel>
                   <Select
+                    isRequired
                     onChange={(e) => setService({
                       ...service, quantity_installments: Number(e.target.value),
                     })}
@@ -216,13 +293,21 @@ export const NewService: React.FC = () => {
                 </InputGroup>
               </GridItem>
               <GridItem
-                display={service.form_of_payment === 'Parcelamento' ? 'block' : 'none'}
+                display={
+                  formOfPayment === 'Parcelamento'
+                  && paymentMethod === 'Cartão de crédito'
+                    ? 'block'
+                    : 'none'
+                }
               >
-                <InputGroup>
+                <InputGroup flexDirection="column">
                   <FormLabel>Dia de vencimento</FormLabel>
                   <Select
-                    onChange={(value) => setService({ ...service, payment_day: Number(value) })}
-                    defaultValue={paymentDays[0]}
+                    isRequired
+                    onChange={(e) => setService({
+                      ...service, payment_day: Number(e.target.value),
+                    })}
+                    defaultValue={Number(service.payment_day)}
                   >
                     {
                       paymentDays.map((day, index) => (
@@ -236,27 +321,28 @@ export const NewService: React.FC = () => {
                 <InputGroup flexDirection="column">
                   <FormLabel>Data do pagamento</FormLabel>
                   <Input
+                    isRequired
                     bg="gray.100"
                     placeholder="Ex: 05/05/2022"
                     type="date"
-                    onChange={(date) => console.log(date)}
+                    onChange={(e) => setService({ ...service, last_payment: e.target.valueAsDate })}
                   />
                 </InputGroup>
               </GridItem>
               <GridItem
                 display={
-                  service.form_of_payment === 'Parcelamento'
-                    && service.total > 0
+                  formOfPayment === 'Parcelamento'
+                    && totalForm > 0
                     ? 'block'
                     : 'none'
                 }
               >
-                <InputGroup>
+                <InputGroup flexDirection="column">
                   <FormLabel>Valor da parcela</FormLabel>
                   <Tag
                     colorScheme="yellow"
                   >
-                    { `R$ ${service.installment_value}` }
+                    { `R$ ${String(installmentValue).replace('.', ',')}` }
                   </Tag>
                 </InputGroup>
               </GridItem>
@@ -267,9 +353,32 @@ export const NewService: React.FC = () => {
                     size="lg"
                     colorScheme="green"
                   >
-                    { `R$ ${Number((service.installment_value * service.quantity_installments).toFixed(2))}` }
+                    { `R$ ${String((service.total).toFixed(2)).replace('.', ',')}` }
                   </Tag>
                 </InputGroup>
+              </GridItem>
+              <GridItem>
+                <Button
+                  disabled={loading}
+                  border="1px solid #2B6CB0"
+                  color="#2B6CB0"
+                  type="submit"
+                >
+                  {
+                    loading
+                      ? (
+                        <Text>
+                          Criando...
+                          <Spinner
+                            size="sm"
+                            emptyColor="gray.200"
+                            color="blue.500"
+                          />
+                        </Text>
+                      )
+                      : 'Criar'
+                  }
+                </Button>
               </GridItem>
             </Grid>
           </form>
