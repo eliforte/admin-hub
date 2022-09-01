f6570549ef5dce1e13338194dce798c024471f09
Elias Forte <eliasforte.dev@gmail.com>
2022-08-31 23:40:42 -0300

    fix(component): loading

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -3,6 +3,7 @@ import { Center, Spinner } from '@chakra-ui/react';
 
 export const Loading: React.FC = () => (
   <Center height="100vh">
-    Carregando <Spinner size="lg" />
+    Carregando
+    <Spinner size="lg" />
   </Center>
 );
