2ab2a88d04f048c933d37479a0e45563c6872bf2
Elias Forte <eliasforte.dev@gmail.com>
2022-09-06 23:35:50 -0300

    style: add margin left

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -4,6 +4,6 @@ import { Center, Spinner } from '@chakra-ui/react';
 export const Loading: React.FC = () => (
   <Center color="blue.600" height="100vh">
     Carregando
-    <Spinner color="blue.600" size="lg" />
+    <Spinner ml="5px" color="blue.600" size="lg" />
   </Center>
 );
